17797d1de2830a6535c915b3fc343dedc00a7d4a
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2023-05-04 10:54:35 +1000

    feed pin created

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -7,16 +7,14 @@ import { Sidebar, UserProfile } from "../components";
 import { client } from "../client";
 import logo from "../assets/berbagi_logo.png";
 import { userQuery } from "../utils/data";
+import { fetchUser } from "../utils/fetchUser";
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?._id);
